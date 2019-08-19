1bbb0bfea0e77d113960dcd9c9bec01b8f70b76f
vinothkumarrenganathan <vinoth.vinothrenga@gmail.com>
2019-08-19 15:05:37 +0530

    wrapper changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,89 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import propTypes from 'prop-types'
+import tinytabs from '@htoniv/tiny-tabs'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let propStr = propTypes.string;
+let propBool = propTypes.boolean;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class ReactTinyTabs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          id: props.id,
+          anchor: props.anchor,
+          closable: props.closable,
+          hideTitle: props.hideTitle,
+          sectionClass: props.sectionClass,
+          titleClass: props.titleClass,
+          tabsClass: props.tabsClass,
+          tabClass: props.tabClass
+        }
+    }
+
+    handleClose(id) {
+        this.props.onClose(id)
+    }
+
+    handleOnBefore(id) {
+        this.props.onBefore(id)
+    }
+
+    handleOnAfter(id) {
+        this.props.onAfter(id)
+    }
+
+    componentDidMount() {
+        let self = this
+        let { anchor, hideTitle, closable, sectionClass, titleClass, tabsClass, tabClass } = this.state
+        tinytabs(document.querySelector("#"+this.state.id), {
+            anchor: anchor,
+            hideTitle: hideTitle,
+            closable: closable,
+            sectionClass: sectionClass,
+            titleClass: titleClass,
+            tabsClass: tabsClass,
+            tabClass: tabClass,
+            onClose: function (id) {
+                self.handleClose(id)
+            },
+            onBefore: function (id, tab) {
+                self.handleOnBefore(id, tab)
+            },
+            onAfter: function (id, tab) {
+                self.handleOnAfter(id, tab)
+            }
+		})
+    }
+    
+    render() {
+        return (
+            <div id={this.state.id}>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+ReactTinyTabs.defaultProps = {
+    id: 'mytabs',
+    anchor: false,
+    closable: true,
+    hideTitle: false,
+    sectionClass: 'section',
+    titleClass: 'title',
+    tabsClass: 'tabs',
+    tabClass: 'tab'
+};
+
+ReactTinyTabs.propTypes = {
+    id: propStr,
+    anchor: propBool,
+    closable: propBool,
+    hideTitle: propBool,
+    sectionClass: propStr,
+    titleClass: propStr,
+    tabsClass: propStr,
+    tabClass: propStr
+};
+
+export default ReactTinyTabs;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  externals: {
+    'react': 'commonjs react'
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.devtool = '#source-map'
+    module.exports.plugins = (module.exports.plugins || []).concat([
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"'
+        }
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true
+      })
+    ])
+  }
